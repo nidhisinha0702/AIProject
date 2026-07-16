12014d124f46b67002ebaec7917e6f8f35a641b5
Nidhi Kumari <nsinha7295@gmail.com>
2026-07-16 17:50:20 +0530

    Adding day2 files

---
+++ week1/day2/sys_temp.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq 
+
+load_dotenv()
+my_api_key=os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key kaha hai bhai?")
+
+client = Groq(api_key=my_api_key)
+
+model="llama-3.3-70b-versatile"
+role="user"
+prompt="suggest a name for my food company"
+#message me role and content
+
+message_system={
+    "role" : "system",
+    "content" : "You are a brand manager who suggest name for my food company"
+}
+message={
+    "role" : role,
+    "content" : prompt
+}
+
+messages=[message_system, message]
+
+response = client.chat.completions.create(model=model, messages=messages, temperature = 1)
+#print(response)
+
+print("###########################")
+
+answer = response.choices[0].message.content
+print(answer)
